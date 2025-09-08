b80b7423fa203adb3ee982d2f0afeaa58619837b
COOL5187 <syedumair.shah47.su@.gmail.com>
2025-09-08 05:46:10 -0700

    hi

---
+++ shapes.py
@@ -0,0 +1,31 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+
+board = turtle.Turtle()
+
+# Triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.up
+board.goto(100 ,280)
+board.down
+
+# Square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
